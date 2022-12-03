2c3c94bc39d6e08e32c62093d5c2bda13971e8f8
a <a@DESKTOP-TU47UA5>
2022-12-04 02:55:32 +0400

    Add project files.

---
+++ Home06 methods matrics/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Home06_methods_matrics
+{
+    class Program
+    {
+        static int[,] Matrix(int n, int a, int b)
+        {
+            int[,] Matrix = new int[n, n];
+            Random r = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Matrix[i, j] = r.Next(a, b);
+                }
+            }
+            return Matrix;
+        }
+        static void PrintMatrix(int[,] Matrix)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    if (i==j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else if (i > j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    Console.Write(Matrix[i, j] + " ");
+                }
+                Console.ResetColor();
+               Console.WriteLine("\n");
+            } 
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Matrix size (n): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter minimum value (a): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter maximum value (b): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            PrintMatrix(Matrix(n, a, b));
+
+            int[,] matrix = Matrix(n, a, b);
+        }
+    }
+}
